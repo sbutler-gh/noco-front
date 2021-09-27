1747fad675a99e83076fffa1668f9ac8f912e719
sbutler-gh <sam@ysli.be>
2021-09-27 15:12:02 +0200

    Updating database schema to use HTML calendar table instead of Markdown table

--- src/lib/TopicCard.svelte
+++ src/lib/TopicCard.svelte
@@ -1,11 +1,33 @@
 <script>
+import { onMount } from "svelte";
+
+
     export let topic;
+
+    let calendar = "loading ...";
+    // The function :)
+
+    // onMount(() => {
+    //     let calendar_raw = topic.sync_calendar_times;
+    //     calendar_raw = calendar_raw.replace("\n", String.Empty);
+    //     calendar = calendar_raw.replace(`^(\|[^\n]+\|\r?\n)((?:\|:?[-]+:?)+\|)(\n(?:\|[^\n]+\|\r?\n?)*)?$`);
+    // });
 </script>
 
 <div class="container px-5 py-5 mx-auto flex">
-    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 m-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+    <div class="bg-white rounded-lg p-8 m-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
 
     {topic.title}
     <iframe src={topic.opening_artifact} />
+    {@html topic.sync_calendar_times}
 </div>
 </div>
+
+<style>
+    .tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+</style>
