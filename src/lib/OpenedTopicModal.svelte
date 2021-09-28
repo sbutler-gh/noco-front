38a126ab092d759f29c9e3587ab98081d28e69b8
sbutler-gh <sam@ysli.be>
2021-09-28 10:44:40 +0200

    Users can click calendar table, update contents, and push updated table to DB

--- src/lib/OpenedTopicModal.svelte
+++ src/lib/OpenedTopicModal.svelte
@@ -32,22 +32,121 @@ function closeModal() {
 
     function handleCalendarClick(e) {
 
-        if (e.target.localName != "input") {
-            if (last_target?.firstChild && last_target != e.target ) {
-            last_target.removeChild(last_target.firstChild);
-             }
+        if (e.target.localName != "input" && e.target.innerText.match(`^(?!.*[0-9])`) && e.target.localName != "th") {
+
+            if (document.getElementById('cell_form')) {
+                document.getElementById('cell_form').remove();
+            }
+            // if (last_target?.firstChild) {
+            // // last_target.removeChild(last_target.firstChild);
+            // // last_target.removeChild(last_target.firstChild);
+            //  }
+        
+        const cellForm = document.createElement("form");
+        // cellForm.onsubmit = handleCalendarCellSubmit;
+        cellForm.id = "cell_form";
 
         const newInput = document.createElement("input");
         newInput.style.border = "solid 1px grey";
+        newInput.id = "cell_input";
+        newInput.value = e.target.innerText;
+        const submitButton = document.createElement('button');
+        submitButton.id = "cell_submit";
+        // submitButton.style.border = "solid 1px grey";
+        submitButton.style.background = "#007cff";
+        submitButton.innerText = "Save";
+        submitButton.style.color = "white";
+        submitButton.style.marginLeft = "auto";
+        submitButton.style.marginTop = "3px";
+        submitButton.style.display = "block";
+        submitButton.style.borderRadius = "5px";
+        submitButton.style.padding = "3px";
+        // submitButton.classList = "rounded block ml-auto mt-1 text-white bg-blue-500"
+        // newInput.appendChild(submitButton);
+        submitButton.type = 'button';
+        submitButton.onclick = handleCalendarCellSubmit;
+
+        // const cancelButton = document.createElement('button');
+        // cancelButton.id = "cell_cancel";
+        // // submitButton.style.border = "solid 1px grey";
+        // cancelButton.style.background = "lightgrey";
+        // cancelButton.innerText = "x";
+        // cancelButton.style.color = "black";
+        // cancelButton.style.marginLeft = "auto";
+        // cancelButton.style.marginTop = "3px";
+        // cancelButton.style.display = "block";
+        // cancelButton.style.borderRadius = "5px";
+        // cancelButton.style.padding = "3px";
+        // cancelButton.onclick = function() {
+        //     last_target.removeChild(last_target.firstChild);
+        // }
+
 
         console.log(e);
         console.log(e.target);
-        e.target.appendChild(newInput);
+        cellForm.appendChild(newInput);
+        // cellForm.appendChild(cancelButton);
+        cellForm.appendChild(submitButton);
+        e.target.appendChild(cellForm);
+        // e.target.appendChild(newInput);
+        // e.target.appendChild(submitButton);
 
         last_target = e.target;
         // e.target.style.background = "yellow";
 
         }
+    }
+
+    function handleCalendarCellSubmit(e) {
+        console.log(e);
+        let cell_input = document.getElementById('cell_input');
+        console.log(cell_input.value);
+        console.log(last_target);
+        let cell_text = cell_input.value;
+        last_target.textContent = cell_input.value;
+
+        sendUpdatedCalendarToDB();
+        // document.getElementById('cell_form').remove();
+        // cell_input.value;
+        // last_target.removeChild(last_target.firstChild);
+        // last_target.appendChild(cell_text);
+
+        // console.log(docu)
+        // console.log(e.target);
+    }
+
+    async function sendUpdatedCalendarToDB() {
+
+        let new_calendar = document.getElementById('calendar_table').outerHTML;
+
+        console.log(new_calendar);
+
+        const myHeaders = new Headers();
+
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append('xc-token', 'hYIxUCPsWRKc2q3_L0HXWHqpP3gEnPvKN9lhFY_D');
+
+        let body_submission = {
+        "sync_calendar_times": new_calendar
+        }
+
+        console.log(body_submission);
+        console.log(typeof body_submission);
+    
+        const response = await fetch(`http://nocodb-production-58e5.up.railway.app/nc/making_things_real_rest_Eu7R/api/v1/topics/${topic.id}`, {
+                method: 'PUT',
+                body: JSON.stringify(body_submission),
+                withCredentials: true,
+                headers: myHeaders
+                })
+
+        if (response.ok) {
+            
+            let response_json = await response.json();
+            console.log(response_json);
+        }
+
+
     }
 </script>
 
@@ -62,8 +161,8 @@ function closeModal() {
     </div>
 
     <div on:click={handleCalendarClick} class="mt-8 mb-6 m-auto md:w-min">
-        <p class="block mb-2">When are you available to talk about this?</p>
-        <p class="block mb-2">Timezone: CET</p>
+        <p class="block mb-2">When are you available to talk about this? <a href={topic.meeting_room} class="text-underline text-blue-500">(Meeting Room)</a></p>
+        <p class="block mb-2">Initial your name in the calendar blocks below. (Timezone: CET)</p>
         {@html topic.sync_calendar_times}
       </div>
     </div>
